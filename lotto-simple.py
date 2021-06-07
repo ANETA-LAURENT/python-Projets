5f88a9629b529a360ba2c565121fab519b8f234d
aneta <65393573+ANETA-LAURENT@users.noreply.github.com>
2021-06-07 16:33:53 +0200

    Update lotto-simple.py

--- lotto-simple.py
+++ lotto-simple.py
@@ -1,15 +1,20 @@
+""" Ecrivez un programme pour simuler le lotto. Le programme implémente une liste de 7 nombres, qui représente
+un ticket de lotto. Le programme va ensuite générer 7 nombres aléatoires et comparer au ticket.
+Le programme affiche ensuite la somme gagnée en suivant les règles ci dessous (en fonction de combien de
+nombres correspondent entre le ticket et les nombres tirés)● 20 euros si 3 nombres correspondent ect """
+
 import random as r
 
 
 def lotto():
     print("Pour jouer en lotto choisissez 7 numéros entre 1 à 49: ")
-    pull = range(1, 12)
+    pull = range(1, 49)
     your_choice = []
     tirage = r.sample(pull, 7)
 
     while len(your_choice) < 7:
         your_numbers = int(input("Votre numero: "))
-        if 1 <= your_numbers <= 12 and your_numbers not in your_choice:
+        if 1 <= your_numbers <= 48 and your_numbers not in your_choice:
             your_choice.append(your_numbers)
 
     your_choice.sort()
